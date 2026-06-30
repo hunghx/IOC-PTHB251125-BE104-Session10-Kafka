36ef698d0ce0ae0bae3ad9f5395d93210ef6c483
hunghx <baby05051999@gmail.com>
2026-06-30 21:11:52 +0700

    init

--- notification-service/src/main/java/com/example/notificationservice/NotificationConsumer.java
+++ notification-service/src/main/java/com/example/notificationservice/NotificationConsumer.java
@@ -1,19 +1,25 @@
 package com.example.notificationservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class NotificationConsumer {
+    private final ObjectMapper objectMapper;
 
     // tạo phương thức lắng nghe event
     @KafkaListener(topics = "order-events-topic" , groupId = "notification-group")
     public void consumerOrderEvent(String value){
-        log.info("Receiving Dataa .......");
-        log.info("Logging Info : Customer Name = {}", value);
 
+        log.info("Receiving Dataa .......");
+        log.info("Logging Info : Data = {}", value);
+        OrderEvent orderEvent = objectMapper.readValue(value, OrderEvent.class);
+        log.info("Logging Info : CustomerName = {}", orderEvent.getCustomerName());
         log.info("Handle other action .....");
     }
 }
--- order-service/src/main/java/com/example/orderservice/OrderProducer.java
+++ order-service/src/main/java/com/example/orderservice/OrderProducer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 
@@ -11,15 +12,21 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderProducer {
+    private final ObjectMapper objectMapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
     public void sendOrderEvent(String orderId, String customerName, String customerEmail, BigDecimal totalAmount){
-//        OrderEvent orderEvent = new OrderEvent();
-//        orderEvent.setOrderId(orderId);
-//        orderEvent.setCustomerName(customerName);
-//        orderEvent.setCustomerEmail(customerEmail);
-//        orderEvent.setTotalAmount(totalAmount);
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setOrderId(orderId);
+        orderEvent.setCustomerName(customerName);
+        orderEvent.setCustomerEmail(customerEmail);
+        orderEvent.setTotalAmount(totalAmount);
         log.info("Sending order event to Kafka with orderId :  {}", orderId);
-        kafkaTemplate.send("order-events-topic",orderId, customerName);
+        // Khi dùng producer để gửi 1 Object thì nó còn gửi thêm header chưa tên package
+        // Chuyển đổi từ Object và String
+//        ObjectMapper objectMapper = new ObjectMapper();
+        String stringData = objectMapper.writeValueAsString(orderEvent);
+
+        kafkaTemplate.send("order-events-topic",orderId, stringData);
         log.info("Send event successfully !!!!");
     }
 }
